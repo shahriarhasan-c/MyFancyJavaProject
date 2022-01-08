80db033602d33fcd0e618c306a6ea47f373f3ef2
tahsin <thasintaha1234@gmail.com>
2022-01-08 20:33:08 +0600

    singleton class added

--- simple.java
+++ simple.java
@@ -1,22 +1,50 @@
 import java.util.Scanner;
-  
-class SwapTwoNumberWithoutThirdVariable
-{
-   public static void main(String args[])
-   {
-      int x, y;
+
+
+class Singleton {
+    private static Singleton single_instance = null;
+ 
+    
+      public int a, b;
       System.out.println("Enter x and y");
       Scanner in = new Scanner(System.in);
   
-      x = in.nextInt();
-      y = in.nextInt();
+      
+    private Singleton()
+    {
+      a = in.nextInt();
+      b = in.nextInt();
+    }
+ 
+    public static Singleton Singleton()
+    {
+        
+        if (single_instance == null) {
+            single_instance = new Singleton();
+        }
+        return single_instance;
+    }
+}
+ 
+
+class simple {
+    
+    public static void main(String args[])
+    {
+        
+        Singleton x = Singleton.Singleton();
+ 
+        
+ 
+      System.out.println("Before Swapping\nx = "+x.a+"\ny = "+x.b);
   
-      System.out.println("Before Swapping\nx = "+x+"\ny = "+y);
+      x.a = x.a + x.b;
+      x.b = x.a - x.b;
+      x.a = x.a - x.b;
   
-      x = x + y;
-      y = x - y;
-      x = x - y;
-  
-      System.out.println("After Swapping without third variable\nx = "+x+"\ny = "+y);
-   }
+      System.out.println("After Swapping without third variable\nx = "+x.a+"\ny = "+x.b);
+ 
+        
+        
+    }
 }
