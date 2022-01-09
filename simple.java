848d7b017955ffe7a7216c11074e94cc03f58a94
Shahriar Hasan <sh.shahriar5@gmail.com>
2022-01-09 16:33:12 +0600

    Now x is c and y is d

--- simple.java
+++ simple.java
@@ -4,19 +4,19 @@ class SwapTwoNumberWithoutThirdVariable
 {
    public static void main(String args[])
    {
-      int x, y;
+      int c, d;
       System.out.println("Enter x and y");
       Scanner in = new Scanner(System.in);
   
-      x = in.nextInt();
-      y = in.nextInt();
+      c = in.nextInt();
+      d = in.nextInt();
   
-      System.out.println("Before Swapping\nx = "+x+"\ny = "+y);
+      System.out.println("Before Swapping\nx = "+c+"\ny = "+d);
   
-      x = x + y;
-      y = x - y;
-      x = x - y;
+      c = c + d;
+      d = c - d;
+      c = c - d;
   
-      System.out.println("After Swapping without third variable\nx = "+x+"\ny = "+y);
+      System.out.println("After Swapping without third variable\nx = "+c+"\ny = "+d);
    }
 }
